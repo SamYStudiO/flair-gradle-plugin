4974c08e8dd28704fb17d940ec7c8fa758b458b5
SamYStudiO <contact@samystudio.net>
2016-01-16 22:37:15 +0100

    Removed .idea

---
+++ src/main/groovy/flair/gradle/executable/IExecutable.groovy
@@ -0,0 +1,11 @@
+package flair.gradle.executable
+
+/**
+ * @author SamYStudiO ( contact@samystudio.net )
+ */
+interface IExecutable
+{
+	public String getExecutable()
+
+	public List<String> getArguments()
+}
