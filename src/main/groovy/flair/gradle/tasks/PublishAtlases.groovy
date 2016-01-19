94f7d75885628a7dea7ca653b36c8a16ce5bc373
SamYStudiO <contact@samystudio.net>
2016-01-19 21:41:30 +0100

    Made PublishAtlases a variant task

--- src/main/groovy/flair/gradle/tasks/PublishAtlases.groovy
+++ src/main/groovy/flair/gradle/tasks/PublishAtlases.groovy
@@ -8,7 +8,7 @@ import org.gradle.api.tasks.TaskAction
 /**
  * @author SamYStudiO ( contact@samystudio.net )
  */
-class PublishAtlases extends AbstractFlairTask
+class PublishAtlases extends AbstractVariantTask
 {
 	public PublishAtlases()
 	{
@@ -22,45 +22,60 @@ class PublishAtlases extends AbstractFlairTask
 		String moduleName = extensionManager.getFlairProperty( "moduleName" )
 		boolean generateATFTexturesFromAtlases = extensionManager.getFlairProperty( "texturepacker" , "generateATFTexturesFromAtlases" )
 
-		String toATF = ""
-		FileTree tree = project.fileTree( "${ moduleName }/src/main/" )
+		List<String> list = new ArrayList<String>( )
 
-		tree.each { file ->
+		list.add( "main" )
+		list.add( variant.platform.name )
 
-			if( file.getName( ).toLowerCase( ).indexOf( ".tps" ) >= 0 )
-			{
-				ByteArrayOutputStream output = new ByteArrayOutputStream( )
-
-				project.exec {
-					executable Os.isFamily( Os.FAMILY_WINDOWS ) ? "texturepacker" : "/usr/local/bin/texturepacker"
-					args file.getPath( )
-					ignoreExitValue = true
-
-					standardOutput = output
-				}
-
-				if( output.toString( ).indexOf( "Output files are newer than the input files, nothing to do" ) < 0 ) toATF += file.getName( ).toLowerCase( ).split( "\\." )[ 0 ]
-			}
+		variant.productFlavors.each {
+			list.add( it )
 		}
 
-		if( generateATFTexturesFromAtlases )
-		{
-			String png2atf = "${ new Sdk( project ).path }/atftools/png2atf"
+		list.add( variant.buildType )
 
-			tree = project.fileTree( "${ moduleName }/src/main/resources" ) {
-				include "drawable*/**/*.png"
-			}
+		String toATF = ""
+
+		list.each {
+
+			FileTree tree = project.fileTree( "${ moduleName }/src/${ it }/" )
 
 			tree.each { file ->
 
-				if( toATF.indexOf( file.getName( ).toLowerCase( ).replaceAll( "[0-9]+\\." , "\\." ).split( "\\." )[ 0 ] ) >= 0 )
+				if( file.getName( ).toLowerCase( ).indexOf( ".tps" ) >= 0 )
 				{
-					String input = file.getAbsolutePath( )
-					String output = input.replaceAll( "\\.png" , "\\.atf" )
+					ByteArrayOutputStream output = new ByteArrayOutputStream( )
 
 					project.exec {
-						commandLine "${ png2atf }" , "-c" , "e2" , "-n" , "0,0" , "-r" , "-i" , input , "-o" , output
+						executable Os.isFamily( Os.FAMILY_WINDOWS ) ? "texturepacker" : "/usr/local/bin/texturepacker"
+						args file.getPath( )
 						ignoreExitValue = true
+
+						standardOutput = output
+					}
+
+					if( output.toString( ).indexOf( "Output files are newer than the input files, nothing to do" ) < 0 ) toATF += file.getName( ).toLowerCase( ).split( "\\." )[ 0 ]
+				}
+			}
+
+			if( generateATFTexturesFromAtlases )
+			{
+				String png2atf = "${ new Sdk( project ).path }/atftools/png2atf"
+
+				tree = project.fileTree( "${ moduleName }/src/${ it }/resources" ) {
+					include "drawable*/**/*.png"
+				}
+
+				tree.each { file ->
+
+					if( toATF.indexOf( file.getName( ).toLowerCase( ).replaceAll( "[0-9]+\\." , "\\." ).split( "\\." )[ 0 ] ) >= 0 )
+					{
+						String input = file.getAbsolutePath( )
+						String output = input.replaceAll( "\\.png" , "\\.atf" )
+
+						project.exec {
+							commandLine "${ png2atf }" , "-c" , "e2" , "-n" , "0,0" , "-r" , "-i" , input , "-o" , output
+							ignoreExitValue = true
+						}
 					}
 				}
 			}
---
+++ src/main/groovy/flair/gradle/tasks/variantFactories/PublishAtlasesTaskFactory.groovy
@@ -0,0 +1,26 @@
+package flair.gradle.tasks.variantFactories
+
+import flair.gradle.tasks.Group
+import flair.gradle.tasks.PublishAtlases
+import flair.gradle.variants.Variant
+import org.gradle.api.Project
+
+/**
+ * @author SamYStudiO ( contact@samystudio.net )
+ */
+public class PublishAtlasesTaskFactory implements IVariantTaskFactory<PublishAtlases>
+{
+	public PublishAtlases create( Project project , Variant variant )
+	{
+		String name = "publishAtlases" + variant.name
+
+		PublishAtlases t = project.tasks.findByName( name ) as PublishAtlases
+
+		if( !t ) t = project.tasks.create( name , PublishAtlases )
+
+		t.group = Group.TEXTURE_PACKER.name
+		t.variant = variant
+
+		return t
+	}
+}
