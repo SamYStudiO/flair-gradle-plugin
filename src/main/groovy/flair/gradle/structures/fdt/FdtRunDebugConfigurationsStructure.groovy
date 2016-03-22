db91bd0515ad0124f6e94c1ea96d421c39044f47
SamYStudiO <contact@samystudio.net>
2016-03-22 22:17:21 +0100

    Fixed compilation from run/debug with FDT

--- src/main/groovy/flair/gradle/structures/fdt/FdtRunDebugConfigurationsStructure.groovy
+++ src/main/groovy/flair/gradle/structures/fdt/FdtRunDebugConfigurationsStructure.groovy
@@ -1,13 +1,15 @@
 package flair.gradle.structures.fdt
 
+import flair.gradle.cli.Adl
+import flair.gradle.cli.ICli
+import flair.gradle.dependencies.Config
 import flair.gradle.extensions.FlairProperty
 import flair.gradle.extensions.IExtensionManager
 import flair.gradle.plugins.PluginManager
 import flair.gradle.structures.IStructure
-import flair.gradle.tasks.TaskDefinition
 import flair.gradle.utils.Platform
-import flair.gradle.utils.Variant.NamingType
 import org.gradle.api.Project
+import org.gradle.api.file.FileTree
 
 /**
  * @author SamYStudiO ( contact@samystudio.net )
@@ -30,7 +32,7 @@ public class FdtRunDebugConfigurationsStructure implements IStructure
 		project.file( ".settings/launch" ).listFiles( ).each { list.add( it.name ) }
 
 		String template = project.file( "${ source.path }/fdt/run_debug_template.xml" ).text
-		String gradleTemplate = project.file( "${ source.path }/fdt/gradle_launch_template.xml" ).text
+		//String gradleTemplate = project.file( "${ source.path }/fdt/gradle_launch_template.xml" ).text
 
 		IExtensionManager flair = project.flair as IExtensionManager
 
@@ -58,23 +60,38 @@ public class FdtRunDebugConfigurationsStructure implements IStructure
 				}
 
 				String app = buildPathFromRoot( project.file( "${ project.buildDir.path }/${ variant.name }/app_descriptor.xml" ).path )
-				String mainClass = buildPathFromRoot( project.file( "${ project.buildDir.path }/${ variant.name }/classes/${ ( extensionManager.getFlairProperty( variant , FlairProperty.COMPILER_MAIN_CLASS ) as String ).split( "\\." ).join( "/" ) }.as" ).path )
+				String mainClassName = ( extensionManager.getFlairProperty( variant , FlairProperty.COMPILER_MAIN_CLASS ) as String ).split( "\\." ).last( ) + ".as"
+				String mainClass
+
+				project.configurations.findAll { it.name.toLowerCase( ).contains( Config.SOURCE.name ) }.each {
+
+					it.files.each { file ->
+
+						FileTree tree = project.fileTree( file )
+
+						tree.each { classFile ->
+
+							if( classFile.name == mainClassName ) mainClass = buildPathFromRoot( classFile.path )
+						}
+					}
+				}
+
+				//String mainClass = buildPathFromRoot( project.file( "${ project.buildDir.path }/${ variant.name }/classes/${ ( extensionManager.getFlairProperty( variant , FlairProperty.COMPILER_MAIN_CLASS ) as String ).split( "\\." ).join( "/" ) }.as" ).path )
 				String sdk = extensionManager.getFlairProperty( variant , FlairProperty.PACKAGE_PLATFORM_SDK ) as String ?: ""
 				String screenSize = flair.getFlairProperty( variant , FlairProperty.ADL_SCREEN_SIZE ) ?: ""
 				String adlArguments = flair.getFlairProperty( variant , FlairProperty.ADL_PARAMETERS ) ?: ""
 				String profileName = variant.name
 				String sampler = flair.getFlairProperty( variant , FlairProperty.PACKAGE_SAMPLER )
-				String gradlePrepareTask = TaskDefinition.PREPARE_PACKAGE.name + variant.getName( NamingType.CAPITALIZE )
+				//String gradlePrepareTask = TaskDefinition.PREPARE_PACKAGE.name + variant.getName( NamingType.CAPITALIZE )
 
-				String content = gradleTemplate.replaceAll( "\\{gradlePrepareTask\\}" , gradlePrepareTask )
+				/*String content = gradleTemplate.replaceAll( "\\{gradlePrepareTask\\}" , gradlePrepareTask )
 						.replaceAll( "\\{projectName\\}" , project.name )
 
 				File f = project.file( ".settings/launch/${ gradlePrepareTask }.launch" )
 				f.createNewFile( )
-				f.write( content )
+				f.write( content )*/
 
 				List<String> constants = new ArrayList<String>( )
-
 
 				PluginManager.getCurrentPlatforms( project ).each {
 
@@ -91,9 +108,45 @@ public class FdtRunDebugConfigurationsStructure implements IStructure
 					constants.add( "<listEntry value=\"BUILD_TYPE::${ it.toUpperCase( ) }!${ it == variant.buildType }\"/>" )
 				}
 
-				content = template.replaceAll( "\\{app\\}" , app )
+				String pubId = extensionManager.getFlairProperty( variant , FlairProperty.ADL_PUB_ID )
+				boolean noDebug = extensionManager.getFlairProperty( variant , FlairProperty.ADL_NO_DEBUG )
+				boolean atLogin = extensionManager.getFlairProperty( variant , FlairProperty.ADL_AT_LOGIN )
+				List<String> parameters = extensionManager.getFlairProperty( variant , FlairProperty.ADL_PARAMETERS ) as List<String>
+
+				ICli cli = new Adl( )
+				cli.addArgument( "-profile" )
+				cli.addArgument( variant.platform == Platform.DESKTOP ? "extendedDesktop" : "mobileDevice" )
+
+				if( pubId ) cli.addArgument( "-pubid ${ pubId }" )
+				if( noDebug ) cli.addArgument( "-noDebug" )
+				if( atLogin ) cli.addArgument( "-atlogin" )
+
+				if( variant.platform != Platform.DESKTOP )
+				{
+					cli.addArgument( "-screensize" )
+					cli.addArgument( extensionManager.getFlairProperty( variant , FlairProperty.ADL_SCREEN_SIZE ).toString( ) )
+					cli.addArgument( "-XscreenDPI" )
+					cli.addArgument( extensionManager.getFlairProperty( variant , FlairProperty.ADL_SCREEN_DPI ).toString( ) )
+					cli.addArgument( "-XversionPlatform" )
+					cli.addArgument( variant.platform == Platform.IOS ? "IOS" : "AND" )
+				}
+
+				cli.addArgument( "-extdir" )
+				cli.addArgument( project.file( "${ project.buildDir.path }/${ variant.name }/extracted_extensions" ).path )
+
+				cli.addArgument( "${ project.buildDir.path }/${ variant.name }/package/app_descriptor.xml" )
+				cli.addArgument( project.file( "${ project.buildDir.path }/${ variant.name }/package" ).path )
+
+				if( parameters.size( ) )
+				{
+					cli.addArgument( "--" )
+					cli.addArguments( parameters )
+				}
+
+				String content = template.replaceAll( "\\{app\\}" , app )
 						.replaceAll( "\\{screenSize\\}" , screenSize )
 						.replaceAll( "\\{adlArguments\\}" , adlArguments )
+						.replaceAll( "\\{adl\\}" , cli.arguments.join( " " ).replaceAll( "\\\\" , "/" ) )
 						.replaceAll( "\\{constants\\}" , constants.join( System.lineSeparator( ) ) )
 						.replaceAll( "\\{mainClass\\}" , mainClass )
 						.replaceAll( "\\{sdk\\}" , sdk )
@@ -101,9 +154,9 @@ public class FdtRunDebugConfigurationsStructure implements IStructure
 						.replaceAll( "\\{sampler\\}" , sampler )
 						.replaceAll( "\\{platform\\}" , platform )
 						.replaceAll( "\\{profileName\\}" , profileName )
-						.replaceAll( "\\{gradlePrepareTask\\}" , gradlePrepareTask )
+				//.replaceAll( "\\{gradlePrepareTask\\}" , gradlePrepareTask )
 
-				f = project.file( ".settings/launch/${ name }.launch" )
+				File f = project.file( ".settings/launch/${ name }.launch" )
 				f.createNewFile( )
 				f.write( content )
 			}
